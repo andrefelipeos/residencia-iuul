f304c2a3566050221735e7e92996fea341a004f0
André Felipe <andre.felipe.2048@gmail.com>
2024-03-20 21:25:16 -0300

    Cria ConsultasService com método para agendar consultas

--- desafio-1.2/controller/ConsultasController.js
+++ desafio-1.2/controller/ConsultasController.js
@@ -1,13 +1,13 @@
-import { Consulta } from "../model/Consulta.js";
 import { ConsultasRepository } from "../repository/ConsultasRepository.js";
+import { ConsultasService } from "../service/ConsultasService.js";
 
 export class ConsultasController {
   #consultasRepository = new ConsultasRepository();
+  #consultasService = new ConsultasService();
 
   cadastrarAgendamento(cpfDoPaciente, dataStr, horaInicial, horaFinal) {
-    const data = new Date(dataStr.replace(/(\d{2})\/(\d{2})\/(\d{4})/g,"\$2\/\$1\/\$3"));
-    const consulta = new Consulta(cpfDoPaciente, data, horaInicial, horaFinal);
-    this.#consultasRepository.cadastrar(consulta);
+    const data = new Date(dataStr.replace(/(\d{2})\/(\d{2})\/(\d{4})/g, "\$2\/\$1\/\$3"));
+    this.#consultasService.agendarConsulta(cpfDoPaciente, data, horaInicial, horaFinal);
   }
 
   removerAgendamento(cpfDoPaciente, data, horaInicial) {

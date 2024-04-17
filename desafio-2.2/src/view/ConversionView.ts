f27b568dce51a92d74ad5f716210b21cc5e01428
André Felipe <andre.felipe.2048@gmail.com>
2024-04-17 17:42:28 -0300

    Ajusta mensagens de prompt na hora de ler dados

--- desafio-2.2/src/view/ConversionView.ts
+++ desafio-2.2/src/view/ConversionView.ts
@@ -6,16 +6,17 @@ export class ConversionView {
   constructor(private readonly inputReader: InputReader) {}
 
   public async askIfUserWantsToContinue(): Promise<boolean> {
-    const promptMessage: string = `1 - SAIR   2 - CONTINUAR`;
+    const promptMessage: string = "Digite '1' para fazer uma nova conversão."
+      + "\nQualquer outra coisa encerrará o programa.\n> ";
     const input: string = await this.inputReader.getString(promptMessage);
-    if (input === "SAIR") return false;
-    else return true;
+    if (input === "1") return true;
+    else return false;
   }
 
   public async getUserInput(): Promise<ConversionData> {
-    const initialCurrency = await this.inputReader.getString();
-    const targetCurrency = await this.inputReader.getString();
-    const amount = await this.inputReader.getNumber();
+    const initialCurrency = await this.inputReader.getString("Moeda origem: ");
+    const targetCurrency = await this.inputReader.getString("Moeda destino: ");
+    const amount = await this.inputReader.getNumber("Valor: ");
     return { initialCurrency, targetCurrency, amount };
   }
 

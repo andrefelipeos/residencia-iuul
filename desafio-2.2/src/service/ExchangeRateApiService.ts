990e9978d0db2fd17a00364b34949da35dc0150f
André Felipe <andre.felipe.2048@gmail.com>
2024-04-17 20:07:01 -0300

    Refatora service da API para retornar um FulfilledConversionData

--- desafio-2.2/src/service/ExchangeRateApiService.ts
+++ desafio-2.2/src/service/ExchangeRateApiService.ts
@@ -1,14 +1,24 @@
 import "dotenv/config";
 
 import { ConversionData } from "../model/ConversionData";
+import { FulfilledConversionData } from "../model/FulfilledConversionData";
 
 export class ExchangeRateApiService {
-  API_KEY = process.env.EXCHANGE_RATE_API_KEY;
-  API_URL = `https://v6.exchangerate-api.com/v6/${this.API_KEY}/pair`;
+  private API_KEY: string = process.env.EXCHANGE_RATE_API_KEY;
+  private API_URL: string = `https://v6.exchangerate-api.com/v6/${this.API_KEY}/pair`;
 
-  async convert(conversionData: ConversionData) {
-    return await fetch(`${this.API_URL}/${conversionData.initialCurrency}/${conversionData.targetCurrency}/${conversionData.amount}`)
-      .then(response => response.json());
+  public async convert(requestData: ConversionData): Promise<FulfilledConversionData> {
+    return fetch(`${this.API_URL}/${requestData.initialCurrency}/${requestData.targetCurrency}/${requestData.amount}`)
+      .then(response => response.json())
+      .then(json => {
+        return {
+          initialCurrency: json.base_code,
+          targetCurrency: json.target_code,
+          amount: requestData.amount,
+          conversionRate: json.conversion_rate,
+          conversionResult: json.conversion_result
+        } as FulfilledConversionData;
+      });
   }
 }
 

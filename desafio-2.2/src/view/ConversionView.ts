f7aecf3227a7326a34b16026ebb888f73c187dc1
André Felipe <andre.felipe.2048@gmail.com>
2024-04-18 10:22:21 -0300

    Cria métodos em ConversionView para validar entradas do usuário
    Validações implementadas:
    - Os códigos de moedas tem exatamente três letras.
    - O código da moeda destino deve ser diferente do código da moeda inicial.
    - O valor a ser convertido deve ser maior que zero (positivo não-nulo).

--- desafio-2.2/src/view/ConversionView.ts
+++ desafio-2.2/src/view/ConversionView.ts
@@ -16,10 +16,32 @@ export class ConversionView {
     }
   }
 
+  private async getAmountValue(promptMessage: string): Promise<number> {
+    let amountValue: number = await this.inputReader.getNumber(promptMessage);
+    while (amountValue <= 0) {
+      console.log("O valor a ser convertido deve ser maior que zero!");
+      amountValue = await this.inputReader.getNumber(promptMessage);
+    }
+    return amountValue;
+  }
+
+  private async getCurrencyCode(promptMessage: string): Promise<string> {
+    let currencyCode: string = await this.inputReader.getString(promptMessage);
+    while (currencyCode.length !== 3) {
+      console.log("O código de moeda deve conter exatamente três letras!");
+      currencyCode = await this.inputReader.getString(promptMessage);
+    }
+    return currencyCode;
+  }
+
   public async getUserInput(): Promise<ConversionData> {
-    const initialCurrency = await this.inputReader.getString("Moeda origem: ");
-    const targetCurrency = await this.inputReader.getString("Moeda destino: ");
-    const amount = await this.inputReader.getNumber("Valor: ");
+    const initialCurrency: string = await this.getCurrencyCode("Moeda origem: ");
+    let targetCurrency: string = await this.getCurrencyCode("Moeda destino: ");
+    while (targetCurrency === initialCurrency) {
+      console.log("A moeda destino deve ser diferente da moeda inicial!");
+      targetCurrency = await this.getCurrencyCode("Moeda destino: ");
+    }
+    const amount: number = await this.getAmountValue("Valor: ");
     return { initialCurrency, targetCurrency, amount };
   }
 

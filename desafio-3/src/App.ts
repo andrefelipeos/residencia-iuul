76923ec6761ec45f30ad52e4c0bb9c8cd423850a
André Felipe <andre.felipe.2048@gmail.com>
2024-05-15 17:26:55 -0300

    Cria uma conexão de teste com o banco de dados

--- desafio-3/src/App.ts
+++ desafio-3/src/App.ts
@@ -1,4 +1,23 @@
+import "dotenv/config";
+import { Dialect, Sequelize } from "sequelize";
 import { MenuPrincipal } from "./view/MenuPrincipal.js";
+
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT as Dialect,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("A conexão foi estabelecida com sucesso.");
+} catch (erro) {
+  console.error("Incapaz de conectar ao banco de dados", erro);
+}
+
+sequelize.close();
 
 const menu = new MenuPrincipal();
 await menu.iniciar();
